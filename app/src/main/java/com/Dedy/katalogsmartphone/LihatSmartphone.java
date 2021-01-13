23b351532ba0c6da400ff305db43b22d94fc2c04
dedykaryadigunawan <dedykaryadigunawan@gmail.com>
2021-01-13 20:10:29 +0700

    Katalog smartphone

---
+++ app/src/main/java/com/Dedy/katalogsmartphone/LihatSmartphone.java
@@ -0,0 +1,47 @@
+package com.Dedy.katalogsmartphone;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LihatSmartphone extends AppCompatActivity {
+    protected Cursor cursor;
+    DataHelper dbHelper;
+    FloatingActionButton ton2;
+    TextView text1, text2, text3, text4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_smartphone);
+
+        dbHelper = new DataHelper(this);
+        text1 = (TextView) findViewById(R.id.textView1);
+        text2 = (TextView) findViewById(R.id.textView2);
+        text3 = (TextView) findViewById(R.id.textView3);
+        text4 = (TextView) findViewById(R.id.textView4);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM namephone WHERE model = '" + getIntent().getStringExtra("nama") + "'", null);
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            cursor.moveToPosition(0);
+            text1.setText(cursor.getString(0).toString());
+            text2.setText(cursor.getString(1).toString());
+            text3.setText(cursor.getString(2).toString());
+            text4.setText(cursor.getString(3).toString());
+        }
+        ton2 = (FloatingActionButton) findViewById(R.id.button1);
+        ton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/Dedy/katalogsmartphone/TambahSmartphone.java
@@ -0,0 +1,55 @@
+package com.Dedy.katalogsmartphone;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class TambahSmartphone extends AppCompatActivity {
+    protected Cursor cursor;
+    DataHelper dbHelper;
+    FloatingActionButton ton1, ton2;
+    EditText text1, text2, text3, text4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_smartphone);
+
+        dbHelper = new DataHelper(this);
+        text1 = (EditText) findViewById(R.id.editText1);
+        text2 = (EditText) findViewById(R.id.editText2);
+        text3 = (EditText) findViewById(R.id.editText3);
+        text4 = (EditText) findViewById(R.id.editText4);
+        ton1 = (FloatingActionButton) findViewById(R.id.button1);
+        ton2 = (FloatingActionButton) findViewById(R.id.button2);
+
+        ton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.execSQL("insert into namephone(brand, model, spesifikasi, harga) values('" +
+                        text1.getText().toString() + "','" +
+                        text2.getText().toString() + "','" +
+                        text3.getText().toString() + "','" +
+                        text4.getText().toString() + "')");
+                Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_LONG).show();
+                MainActivity.ma.RefreshList();
+                finish();
+            }
+        });
+        ton2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                finish();
+            }
+        });
+    }
+}
